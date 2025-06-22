855485818612a0b68efdb827995c89cf39573126
Santiago V. Scacciaferro W. <91495940+SaintWyss@users.noreply.github.com>
2025-06-22 13:32:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import torch
+import torch.nn.functional as F
 from torchvision.utils import make_grid
 import matplotlib.pyplot as plt
 import numpy as np
-from vae_model import VAE  # asegurate de que esté en el mismo folder
+from vae_model import VAE
 
 st.title("Handwritten Digit Generator (MNIST)")
 
@@ -18,7 +19,9 @@ with torch.no_grad():
     fig, ax = plt.subplots(1, 5, figsize=(10, 2))
     for i in range(5):
         z = torch.randn(1, 20)
-        sample = model.decode(z).view(28, 28).numpy()
+        digit_tensor = F.one_hot(torch.tensor([digit]), num_classes=10).float()
+        z_cond = torch.cat((z, digit_tensor), dim=1)
+        sample = model.decode(z_cond).view(28, 28).numpy()
         ax[i].imshow(sample, cmap="gray")
         ax[i].axis('off')
     st.pyplot(fig)
